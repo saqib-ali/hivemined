cdd46efd0873bc0af40157d5f34a1dfbbdc1f0f2
Saqib Ali <docbook.xml@gmail.com>
2021-06-19 06:42:35 +0000

    init

--- app.py
+++ app.py
@@ -12,28 +12,6 @@ DBCONNSTR=os.environ['DBCONNSTR']
 #postgres://{username}:{password}@free-tier4.aws-us-west-2.cockroachlabs.cloud:26257/{clustername}.defaultdb?sslmode=require
 
 
-job_site_patterns = """\"postUrl\" LIKE '%greenhouse%' 
-    OR \"postUrl\" LIKE '%lever.co%' 
-    OR \"postUrl\" LIKE '%ultipro.com%' 
-    OR \"postUrl\" LIKE '%avature.net%'
-    OR \"postUrl\" LIKE '%workday.com%' 
-    OR \"postUrl\" LIKE '%recruiterbox.com%' 
-    OR \"postUrl\" LIKE '%jobvite.com%' 
-    OR \"postUrl\" LIKE '%applytojob.com%' 
-    OR \"postUrl\" LIKE '%careerarc.com%' 
-    OR \"postUrl\" LIKE '%smartrecruiters.com%' 
-    OR \"postUrl\" LIKE '%recruitee.com%' 
-    OR \"postUrl\" LIKE '%jobscore.com%' 
-    OR \"postUrl\" LIKE '%work4labs.com%' 
-    OR \"postUrl\" LIKE '%jobs.ac.uk%' 
-    OR \"postUrl\" LIKE '%workable.com%' 
-    OR \"postUrl\" LIKE '%brassring.com%' 
-    OR \"postUrl\" LIKE '%softgarden.com%'
-    OR \"postUrl\" LIKE '%careers.%.edu%' 
-    OR \"postUrl\" LIKE '%jobs.%.edu%' 
-    OR \"postUrl\" LIKE '%careers.%.ac.%' 
-    OR \"postUrl\" LIKE '%jobs.%.ac.%' 
-    OR \"postUrl\" LIKE '%jobarxiv.org%'"""
 
 job_site_patterns = """
     \"postUrl\" LIKE '%careers.%.edu%' 
