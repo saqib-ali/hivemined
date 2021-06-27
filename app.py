b68b6e7513829cf99c882f9d4e20c86ecb1e9f68
Saqib Ali <docbook.xml@gmail.com>
2021-06-27 23:22:20 +0000

    updating the feed url

--- app.py
+++ app.py
@@ -24,34 +24,6 @@ DBCONNSTR=os.environ['DBCONNSTR']
 #STORED;
 
 
-job_site_patterns = """
-    
-    \"postUrl\" LIKE '%careers.%.edu%' 
-    OR \"postUrl\" LIKE '%jobs.%.edu%' 
-    OR \"postUrl\" LIKE '%opportunities.%.edu%' 
-    OR \"postUrl\" LIKE '%employment.%.edu%' 
-
-    
-    OR \"postUrl\" LIKE '%careers.%.ac.%' 
-    OR \"postUrl\" LIKE '%jobs.%.ac.%' 
-    OR \"postUrl\" LIKE '%opportunities.%.ac.%' 
-    OR \"postUrl\" LIKE '%employement.%.ac.%' 
-
-    
-    OR \"postUrl\" LIKE '%.ac.%/job%' 
-    OR \"postUrl\" LIKE '%.ac.%/career%'
-    OR \"postUrl\" LIKE '%.ac.%/Vacanc%' 
-
-    OR \"postUrl\" LIKE '%.edu.%/job%' 
-    OR \"postUrl\" LIKE '%.edu.%/career%' 
-    OR \"postUrl\" LIKE '%.edu.%/Vacanc%' 
-    
-    OR \"postUrl\" LIKE '%.edu/job%' 
-    OR \"postUrl\" LIKE '%.edu/career%' 
-    OR \"postUrl\" LIKE '%.edu/Vacanc%' 
-
-    """
-
 
 @app.route('/')
 def index():
