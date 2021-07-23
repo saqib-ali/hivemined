c2bafc5bd59a2f2c7097ebbf90a47f913aa4faf6
Saqib Ali <docbook.xml@gmail.com>
2021-07-23 02:54:24 +0000

    updating the feed url

--- computed_column.sql
+++ computed_column.sql
@@ -2,7 +2,7 @@ ALTER TABLE openstatistics.rssitems DROP COLUMN SHOW_ON_HIVEMINED;
 ALTER TABLE openstatistics.rssitems
 ADD COLUMN SHOW_ON_HIVEMINED INT AS 
 (CASE 
-WHEN "postUrl" LIKE '%careers.%.edu%' OR "postUrl" LIKE '%jobs.%.edu%'  OR "postUrl" LIKE '%opportunities.%.edu%'  OR "postUrl" LIKE '%employment.%.edu%' OR "postUrl" LIKE '%careers.%.ac.%' OR "postUrl" LIKE '%jobs.%.ac.%' OR "postUrl" LIKE '%opportunities.%.ac.%' OR "postUrl" LIKE '%employement.%.ac.%'  OR "postUrl" LIKE '%.ac.%/job%' OR "postUrl" LIKE '%.ac.%/career%' OR "postUrl" LIKE '%.ac.%/Vacanc%'  OR "postUrl" LIKE '%.edu.%/job%' OR "postUrl" LIKE '%.edu.%/career%'  OR "postUrl" LIKE '%.edu.%/Vacanc%' OR "postUrl" LIKE '%.edu/job%' OR "postUrl" LIKE '%.edu/career%'  OR "postUrl" LIKE '%.edu/Vacanc%' OR "postUrl" LIKE '%wvu.taleo.net%'  OR "postUrl" LIKE '%cfopitt.taleo.net%'  OR "postUrl" LIKE 'https://www.jobbnorge.no/en/%' 
+WHEN "postUrl" LIKE '%careers.%.edu%' OR "postUrl" LIKE '%jobs.%.edu%'  OR "postUrl" LIKE '%opportunities.%.edu%'  OR "postUrl" LIKE '%employment.%.edu%' OR "postUrl" LIKE '%careers.%.ac.%' OR "postUrl" LIKE '%jobs.%.ac.%' OR "postUrl" LIKE '%opportunities.%.ac.%' OR "postUrl" LIKE '%employement.%.ac.%'  OR "postUrl" LIKE '%.ac.%/job%' OR "postUrl" LIKE '%.ac.%/career%' OR "postUrl" LIKE '%.ac.%/Vacanc%'  OR "postUrl" LIKE '%.edu.%/job%' OR "postUrl" LIKE '%.edu.%/career%'  OR "postUrl" LIKE '%.edu.%/Vacanc%' OR "postUrl" LIKE '%.edu/job%' OR "postUrl" LIKE '%.edu/career%'  OR "postUrl" LIKE '%.edu/Vacanc%' OR "postUrl" LIKE '%wvu.taleo.net%'  OR "postUrl" LIKE '%cfopitt.taleo.net%'  OR "postUrl" LIKE 'https://www.jobbnorge.no/en/%' OR "postUrl" LIKE '%umb.taleo.net%' 
 THEN 1
 ELSE 0
 END)
